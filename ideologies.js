9ccb2ccec87971dd417cc51a06fbe770d8160e58
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2020-08-25 14:40:48 +0100

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -6,9 +6,9 @@ ideologies = [
             "coop": 75,
             "ownr": 0,
             "glob": 0,
-			"prog": 0,
-			"rebl": 0,
-			"mark": 0
+            "prog": 0,
+            "rebl": 0,
+            "mark": 0
         }
     },
     {
@@ -18,9 +18,9 @@ ideologies = [
             "coop": 50,
             "ownr": 75,
             "glob": 0,
-			"prog": 0,
-			"rebl": 100,
-			"mark": 75
+            "prog": 0,
+            "rebl": 100,
+            "mark": 75
         }
     },
     {
@@ -30,9 +30,9 @@ ideologies = [
             "coop": 25,
             "ownr": 0,
             "glob": 0,
-			"prog": 50,
-			"rebl": 0,
-			"mark": 100
+            "prog": 50,
+            "rebl": 0,
+            "mark": 100
         }
     },
     {
@@ -42,9 +42,9 @@ ideologies = [
             "coop": 25,
             "ownr": 0,
             "glob": 0,
-			"prog": 0,
-			"rebl": 85,
-			"mark": 50
+            "prog": 0,
+            "rebl": 85,
+            "mark": 50
         }
     },
     {
@@ -54,9 +54,9 @@ ideologies = [
             "coop": 50,
             "ownr": 75,
             "glob": 25,
-			"prog": 25,
-			"rebl": 100,
-			"mark": 60
+            "prog": 25,
+            "rebl": 100,
+            "mark": 60
         }
     },
     {
@@ -66,9 +66,21 @@ ideologies = [
             "coop": 0,
             "ownr": 10,
             "glob": 75,
-			"prog": 0,
-			"rebl": 10,
-			"mark": 50
+            "prog": 0,
+            "rebl": 10,
+            "mark": 50
+        }
+    },
+    {
+        "name": "Zapatism",
+        "stats": {
+            "radi": 0,
+            "coop": 75,
+            "ownr": 0,
+            "glob": 40,
+            "prog": 0,
+            "rebl": 0,
+            "mark": 25
         }
     }
 ];
