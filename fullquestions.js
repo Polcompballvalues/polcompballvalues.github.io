65cdf61ab3f6099977a1c092934c84e66c937e92
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2021-05-28 18:29:13 +0100

    General fixes and updates
    Added short and long question options, replaced quark with plender

---
+++ fullquestions.js
@@ -0,0 +1,178 @@
+fullquestions = [
+    {
+        "question": "Sincerity good",
+        "effect": {
+            "spos":10,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Jannies bad",
+        "effect": {
+            "spos":0,
+            "alle":10,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Casual gang",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":10,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Wholsumness good",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":10,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Horny good",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":10,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "I'm active",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":10,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Plenderplar bad",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":10,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "I'm sane",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":10,
+            "rela":0,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "I'm liked",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":10,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "question": "Glowies hurt the server",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":10,
+            "acti":0
+        }
+    },
+    {
+        "question": "I behave well",
+        "effect": {
+            "spos":0,
+            "alle":0,
+            "serv":0,
+            "pers":0,
+            "horn":0,
+            "fame":0,
+            "show":0,
+            "sani":0,
+            "rela":0,
+            "fedp":0,
+            "acti":10
+        }
+    }
+];
---
+++ users.js
@@ -0,0 +1,50 @@
+users = [
+    {
+        "name": "TheGhostOfInky",
+        "stats": {
+            "spos":80,
+            "alle":0,
+            "serv":0,
+            "pers":75,
+            "horn":75,
+            "fame":100,
+            "show":25,
+            "sani":50,
+            "rela":25,
+            "fedp":100,
+            "acti":75
+        }
+    },
+    {
+        "name": "Rayz9989",
+        "stats": {
+            "spos":69,
+            "alle":25,
+            "serv":5,
+            "pers":100,
+            "horn":50,
+            "fame":100,
+            "show":10,
+            "sani":20,
+            "rela":30,
+            "fedp":100,
+            "acti":100
+        }
+    },
+    {
+        "name": "Lord Atom",
+        "stats": {
+            "spos":80,
+            "alle":70,
+            "serv":15,
+            "pers":10,
+            "horn":95,
+            "fame":70,
+            "show":40,
+            "sani":55,
+            "rela":60,
+            "fedp":60,
+            "acti":35
+        }
+    }
+];
