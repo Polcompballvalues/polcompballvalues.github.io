4d71479aef7d54c531d98cba68fcb6ace065b7c7
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2021-06-13 04:17:46 +0100

    Updated to beta 1.1

--- users.js
+++ users.js
@@ -2,161 +2,241 @@ users = [
     {
         "name": "TheGhostOfInky",
         "stats": {
-            "spos":80,
-            "alle":0,
-            "serv":0,
-            "pers":75,
-            "horn":75,
+            "spos":86,
+            "alle":24.2,
+            "serv":11.3,
+            "pers":66,
+            "horn":80.6,
             "fame":100,
-            "show":25,
-            "sani":50,
-            "rela":25,
-            "fedp":100,
-            "acti":75
+            "show":57.7,
+            "sani":10,
+            "rela":63.7,
+            "fedp":87.6,
+            "acti":93.6
         }
     },
     {
         "name": "Rayz9989",
         "stats": {
-            "spos":69,
-            "alle":25,
-            "serv":5,
+            "spos":57.7,
+            "alle":27.3,
+            "serv":46.7,
             "pers":100,
-            "horn":50,
+            "horn":84.7,
             "fame":100,
-            "show":10,
-            "sani":20,
-            "rela":30,
+            "show":0,
+            "sani":46.7,
+            "rela":66.6,
             "fedp":100,
+            "acti":84.6
+        }
+    },
+    {
+        "name": "Founding Froggers",
+        "stats": {
+            "spos":47.4,
+            "alle":63.7,
+            "serv":74,
+            "pers":40.3,
+            "horn":8.3,
+            "fame":98.5,
+            "show":80.8,
+            "sani":74,
+            "rela":97,
+            "fedp":9.2,
+            "acti":13.4
+        }
+    },
+    {
+        "name": "Anne Vuulphie",
+        "stats": {
+            "spos":100,
+            "alle":50,
+            "serv":33,
+            "pers":85.7,
+            "horn":100,
+            "fame":75.5,
+            "show":88.3,
+            "sani":33.3,
+            "rela":69.1,
+            "fedp":76.7,
             "acti":100
         }
     },
     {
-        "name": "Lord Atom",
+        "name": "SteelSmasher",
         "stats": {
-            "spos":80,
-            "alle":70,
-            "serv":15,
-            "pers":10,
-            "horn":95,
-            "fame":70,
-            "show":40,
-            "sani":55,
-            "rela":60,
-            "fedp":60,
-            "acti":35
-        }
-    },
-    {
-        "name": "TechnoSynd",
-        "stats": {
-            "spos":80,
-            "alle":25,
-            "serv":15,
-            "pers":90,
-            "horn":95,
-            "fame":90,
-            "show":95,
-            "sani":95,
-            "rela":25,
-            "fedp":90,
-            "acti":90
-        }
-    },
-    {
-        "name": "FdF",
-        "stats": {
-            "spos":70,
-            "alle":10,
-            "serv":15,
-            "pers":75,
+            "spos":79.6,
+            "alle":40.9,
+            "serv":40.7,
+            "pers":51.4,
             "horn":25,
-            "fame":80,
-            "show":50,
-            "sani":100,
-            "rela":45,
-            "fedp":90,
-            "acti":85
+            "fame":61.4,
+            "show":30.8,
+            "sani":40.7,
+            "rela":66.7,
+            "fedp":84.0,
+            "acti":81.4
         }
     },
     {
-        "name": "Krab Man",
+        "name": "AliceActually",
         "stats": {
-            "spos":60,
-            "alle":40,
-            "serv":55,
-            "pers":30,
-            "horn":15,
+            "spos":69.2,
+            "alle":48.5,
+            "serv":9.3,
+            "pers":70.8,
+            "horn":58.3,
+            "fame":63.6,
+            "show":43.7,
+            "sani":9.3,
+            "rela":48.6,
+            "fedp":93.9,
+            "acti":77.0
+        }
+    },
+    {
+        "name": "StormTigrex",
+        "stats": {
+            "spos":71.4,
+            "alle":2.2,
+            "serv":16.7,
+            "pers":83.3,
+            "horn":30.4,
+            "fame":90,
+            "show":72.3,
+            "sani":16.7,
+            "rela":73.4,
+            "fedp":26.1,
+            "acti":74.3
+        }
+    },
+    {
+        "name": "YSKNM",
+        "stats": {
+            "spos":34.5,
+            "alle":27.3,
+            "serv":62.7,
+            "pers":77.8,
+            "horn":48.6,
+            "fame":7.5,
+            "show":48.7,
+            "sani":62.7,
+            "rela":60.5,
+            "fedp":74.7,
+            "acti":87.2
+        }
+    },
+    {
+        "name": "Slime King",
+        "stats": {
+            "spos":78.2,
+            "alle":57.6,
+            "serv":62,
+            "pers":72.3,
+            "horn":9.7,
             "fame":75,
-            "show":75,
-            "sani":60,
-            "rela":60,
-            "fedp":60,
-            "acti":75
+            "show":64.1,
+            "sani":62,
+            "rela":66.7,
+            "fedp":61.7,
+            "acti":77.6
         }
     },
     {
-        "name": "Chara",
+        "name": "sir grumling",
         "stats": {
-            "spos":90,
-            "alle":10,
-            "serv":5,
-            "pers":80,
-            "horn":65,
-            "fame":90,
-            "show":80,
-            "sani":5,
-            "rela":70,
-            "fedp":90,
-            "acti":80
+            "spos":61.9,
+            "alle":57.7,
+            "serv":76.7,
+            "pers":11.1,
+            "horn":91.8,
+            "fame":19.9,
+            "show":47.3,
+            "sani":76.7,
+            "rela":56.7,
+            "fedp":19.1,
+            "acti":68.3
         }
     },
     {
-        "name": "Jokie",
+        "name": "ItsDanny101",
         "stats": {
-            "spos":70,
-            "alle":45,
-            "serv":30,
-            "pers":60,
-            "horn":35,
-            "fame":80,
-            "show":75,
-            "sani":85,
-            "rela":30,
-            "fedp":40,
-            "acti":50
+            "spos":35.9,
+            "alle":65.1,
+            "serv":61.4,
+            "pers":73.7,
+            "horn":1.4,
+            "fame":75.1,
+            "show":70.5,
+            "sani":61.4,
+            "rela":94,
+            "fedp":57.4,
+            "acti":60.3
         }
     },
     {
-        "name": "Bogg",
+        "name": "grassedasses",
         "stats": {
-            "spos":40,
-            "alle":80,
-            "serv":100,
-            "pers":30,
-            "horn":0,
-            "fame":60,
-            "show":70,
-            "sani":100,
-            "rela":80,
-            "fedp":10,
-            "acti":15
+            "spos":37.2,
+            "alle":59.2,
+            "serv":48.7,
+            "pers":62.5,
+            "horn":86.1,
+            "fame":37.9,
+            "show":51.3,
+            "sani":65,
+            "rela":56,
+            "fedp":51.9,
+            "acti":55.8
         }
     },
     {
-        "name": "Denatidum",
+        "name": "Vizdun",
         "stats": {
-            "spos":60,
-            "alle":0,
-            "serv":50,
-            "pers":70,
-            "horn":25,
-            "fame":70,
-            "show":100,
-            "sani":50,
-            "rela":70,
-            "fedp":50,
-            "acti":80
+            "spos":47.4,
+            "alle":48.5,
+            "serv":76.0,
+            "pers":54.2,
+            "horn":84.7,
+            "fame":61.4,
+            "show":17.9,
+            "sani":81.7,
+            "rela":15.1,
+            "fedp":45.7,
+            "acti":10.9
+        }
+    },
+    {
+        "name": "Mucus Patty",
+        "stats": {
+            "spos":59,
+            "alle":25.8,
+            "serv":54,
+            "pers":75.1,
+            "horn":62.4,
+            "fame":25.7,
+            "show":56.4,
+            "sani":54,
+            "rela":72.8,
+            "fedp":75.4,
+            "acti":82.2
+        }
+    },
+    {
+        "name": "NedoNazBol",
+        "stats": {
+            "spos":94.9,
+            "alle":42.4,
+            "serv":46,
+            "pers":65.3,
+            "horn":12.5,
+            "fame":31,
+            "show":8.9,
+            "sani":46,
+            "rela":78.7,
+            "fedp":54.3,
+            "acti":63.4
         }
     }
 ];
