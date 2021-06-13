99614b755ba30d59e292760ab847516c82b299ff
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2021-06-13 18:09:10 +0100

    Update users.js

--- users.js
+++ users.js
@@ -382,5 +382,245 @@ users = [
             "fedp":57.4,
             "acti":66.7
         }
+    },
+    {
+        "name": "Wasp",
+        "stats": {
+            "spos":57.7,
+            "alle":54.5,
+            "serv":30.6,
+            "pers":58.4,
+            "horn":68,
+            "fame":70.5,
+            "show":74.3,
+            "sani":30.6,
+            "rela":56.1,
+            "fedp":63.6,
+            "acti":75.7
+        }
+    },
+    {
+        "name": "Devil's Fingers",
+        "stats": {
+            "spos":62.9,
+            "alle":48.4,
+            "serv":26.7,
+            "pers":76.5,
+            "horn":36.1,
+            "fame":60.7,
+            "show":46.1,
+            "sani":26.7,
+            "rela":42.5,
+            "fedp":98.1,
+            "acti":93.6
+        }
+    },
+    {
+        "name": "Krab Man",
+        "stats": {
+            "spos":38.1,
+            "alle":65.6,
+            "serv":90,
+            "pers":22.1,
+            "horn":22.2,
+            "fame":70.1,
+            "show":63.8,
+            "sani":90,
+            "rela":60,
+            "fedp":45.2,
+            "acti":59.1
+        }
+    },
+    {
+        "name": "lolman533",
+        "stats": {
+            "spos":45.3,
+            "alle":56.6,
+            "serv":59.9,
+            "pers":52.7,
+            "horn":13.8,
+            "fame":13.2,
+            "show":66.7,
+            "sani":59.9,
+            "rela":70,
+            "fedp":14.3,
+            "acti":51.5
+        }
+    },
+    {
+        "name": "dof247",
+        "stats": {
+            "spos":83.4,
+            "alle":21.3,
+            "serv":9.3,
+            "pers":94.5,
+            "horn":98.5,
+            "fame":98.5,
+            "show":56.5,
+            "sani":9.3,
+            "rela":42.4,
+            "fedp":97.6,
+            "acti":94.9
+        }
+    },
+    {
+        "name": "Based and Jedpilled",
+        "stats": {
+            "spos":70.6,
+            "alle":48.5,
+            "serv":28.6,
+            "pers":80.6,
+            "horn":4.2,
+            "fame":73.5,
+            "show":61.6,
+            "sani":28.6,
+            "rela":91,
+            "fedp":43.2,
+            "acti":64.8
+        }
+    },
+    {
+        "name": "ThatoneguyG",
+        "stats": {
+            "spos":87.2,
+            "alle":90.9,
+            "serv":74.7,
+            "pers":65.3,
+            "horn":95.9,
+            "fame":3,
+            "show":56.4,
+            "sani":74.7,
+            "rela":44,
+            "fedp":90.1,
+            "acti":57.1
+        }
+    },
+    {
+        "name": "Hydra Mystic",
+        "stats": {
+            "spos":59.9,
+            "alle":100,
+            "serv":100,
+            "pers":47.2,
+            "horn":0,
+            "fame":66.7,
+            "show":0,
+            "sani":77.8,
+            "rela":26.6,
+            "fedp":9.5,
+            "acti":1.5
+        }
+    },
+    {
+        "name": "Arvnn Chad",
+        "stats": {
+            "spos":33.3,
+            "alle":80.4,
+            "serv":93.4,
+            "pers":51.4,
+            "horn":16.6,
+            "fame":12.7,
+            "show":35.8,
+            "sani":93.4,
+            "rela":69.8,
+            "fedp":56.9,
+            "acti":51.3
+        }
+    },
+    {
+        "name": "Whiprust",
+        "stats": {
+            "spos":59.6,
+            "alle":39.9,
+            "serv":40.1,
+            "pers":80.6,
+            "horn":83.4,
+            "fame":59.9,
+            "show":24.9,
+            "sani":40.1,
+            "rela":76.8,
+            "fedp":64.3,
+            "acti":81.7
+        }
+    },
+    {
+        "name": "Yves",
+        "stats": {
+            "spos":52.6,
+            "alle":23.7,
+            "serv":52.7,
+            "pers":43,
+            "horn":25,
+            "fame":43.9,
+            "show":30.8,
+            "sani":52.7,
+            "rela":45.5,
+            "fedp":32.1,
+            "acti":79
+        }
+    },
+    {
+        "name": "nguyenreich",
+        "stats": {
+            "spos":62.8,
+            "alle":47,
+            "serv":51.3,
+            "pers":56.9,
+            "horn":52.8,
+            "fame":75,
+            "show":61.5,
+            "sani":51.3,
+            "rela":65.1,
+            "fedp":39.5,
+            "acti":52
+        }
+    },
+    {
+        "name": "Bavyera",
+        "stats": {
+            "spos":11.4,
+            "alle":91.1,
+            "serv":100,
+            "pers":0,
+            "horn":0,
+            "fame":93.3,
+            "show":50,
+            "sani":100,
+            "rela":20,
+            "fedp":0,
+            "acti":0
+        }
+    },
+    {
+        "name": "Spade",
+        "stats": {
+            "spos":37.1,
+            "alle":28.7,
+            "serv":46,
+            "pers":86.2,
+            "horn":57,
+            "fame":25.7,
+            "show":53.9,
+            "sani":33.3,
+            "rela":78.9,
+            "fedp":91.4,
+            "acti":67.9
+        }
+    },
+    {
+        "name": "simon",
+        "stats": {
+            "spos":24.3,
+            "alle":44,
+            "serv":69.4,
+            "pers":41.7,
+            "horn":70.9,
+            "fame":36.6,
+            "show":83.4,
+            "sani":69.4,
+            "rela":86.5,
+            "fedp":79.7,
+            "acti":48.7
+        }
     }
 ];
